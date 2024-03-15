7f788ded7e7836aafec432e87ed06a0975dce647
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2024-03-15 15:55:51 +0300

    add: fonts

--- src/app/film/page.tsx
+++ src/app/film/page.tsx
@@ -1,12 +1,3 @@
-'use client';
-
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: #bf4f74;
-`;
 export default function Home() {
-  return <Title>Film page</Title>;
+  return <h1>FILM PAGE</h1>;
 }
